79f972a75024f672ac0e2619fe143ca15b3373c4
chandermani <chandermani@outlook.com>
2016-03-26 17:57:59 +0530

    Enable module loading with babel.

--- trainer/app/js/app.js
+++ trainer/app/js/app.js
@@ -1,6 +1,19 @@
-'use strict';
+import  './app.module.js';
+import  './config.js';
+import  './root.js';
+import './shared/directives.js';
+import './shared/model.js';
+import './shared/services.js';
+import './7MinWorkout/services.js';
+import './7MinWorkout/directives.js';
+import './7MinWorkout/filters.js';
+import './7MinWorkout/workout.js';
+import './7MinWorkout/workoutvideos.js';
+import './WorkoutBuilder/services.js';
+import './WorkoutBuilder/directives.js';
+import './WorkoutBuilder/exercise.js';
+import './WorkoutBuilder/workout.js';
 
-angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate','ngResource','ngMessages','pascalprecht.translate']);
-
-angular.module('7minWorkout', []);
-angular.module('WorkoutBuilder', []);
+angular.element(document).ready(function() {
+    angular.bootstrap(document.body, ['app'], { strictDi: true });
+});
