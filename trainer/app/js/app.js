f2117b4c07a1b3704c44b7bcb0deb673fedc5d10
chandermani <chandermani@outlook.com>
2016-03-27 13:26:22 +0530

    Module loading enabled with TypeScript

--- trainer/app/js/app.js
+++ trainer/app/js/app.js
@@ -1,19 +1,28 @@
-import  './app.module.js';
-import  './config.js';
-import  './root.js';
-import './shared/directives.js';
-import './shared/model.js';
-import './shared/services.js';
-import './7MinWorkout/services.js';
-import './7MinWorkout/directives.js';
-import './7MinWorkout/filters.js';
-import './7MinWorkout/workout.js';
-import './7MinWorkout/workoutvideos.js';
-import './WorkoutBuilder/services.js';
-import './WorkoutBuilder/directives.js';
-import './WorkoutBuilder/exercise.js';
-import './WorkoutBuilder/workout.js';
-
-angular.element(document).ready(function() {
-    angular.bootstrap(document.body, ['app'], { strictDi: true });
+System.register(['./app.module.js', './config.js', './root.js', './shared/directives.js', './shared/model.js', './shared/services.js', './7MinWorkout/services.js', './7MinWorkout/directives.js', './7MinWorkout/filters.js', './7MinWorkout/workout.js', './7MinWorkout/workoutvideos.js', './WorkoutBuilder/services.js', './WorkoutBuilder/directives.js', './WorkoutBuilder/exercise.js', './WorkoutBuilder/workout.js'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[
+            function (_1) {},
+            function (_2) {},
+            function (_3) {},
+            function (_4) {},
+            function (_5) {},
+            function (_6) {},
+            function (_7) {},
+            function (_8) {},
+            function (_9) {},
+            function (_10) {},
+            function (_11) {},
+            function (_12) {},
+            function (_13) {},
+            function (_14) {},
+            function (_15) {}],
+        execute: function() {
+            angular.element(document).ready(function () {
+                angular.bootstrap(document.body, ['app'], { strictDi: true });
+            });
+        }
+    }
 });
+//# sourceMappingURL=app.js.map
